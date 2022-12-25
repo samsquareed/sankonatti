658e203f0dbc8972825cec8a22e41a087eb80bb8
sammed.sankonatti <sammed.sankonatti@datagrokr.com>
2022-12-25 21:02:17 +0530

    tree

---
+++ src/Tree.js
@@ -0,0 +1,403 @@
+import avatarPersonnel from './assets/avatar-personnel.svg'
+
+export const tree = {
+  id: 100,
+  person: {
+    id: 100,
+    avatar: avatarPersonnel,
+    department: '',
+    name: 'Appu sankonatti',
+    title: 'Head of family!',
+    totalReports: 5,
+  },
+  hasChild: true,
+  hasParent: true,
+  children: [],
+}
+
+export const tree1 = [
+  {
+    id: 36,
+    person: {
+      id: 36,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Ashok Sankonatti',
+      title: 'Farmer',
+      totalReports: 3,
+    },
+    hasChild: true,
+    hasParent: true,
+    // children: [],
+  },
+  {
+    id: 37,
+    person: {
+      id: 37,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Suresh Sankonatti',
+      title: 'Farmer',
+      totalReports: 4,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 25,
+    person: {
+      id: 25,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Ningappa Sankonatti',
+      title: 'Farmer',
+      totalReports: 6,
+    },
+    hasChild: true,
+    hasParent: true,
+    // children: [],
+  },
+  {
+    id: 26,
+    person: {
+      id: 26,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Shreekant Sankonatti',
+      title: 'Farmer',
+      totalReports: 4,
+    },
+    hasChild: true,
+    hasParent: true,
+    // children: [],
+  },
+  {
+    id: 27,
+    person: {
+      id: 27,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Nishkant Sankonatti',
+      title: 'Farmer',
+      totalReports: 1,
+    },
+    hasChild: true,
+    hasParent: true,
+    // children: [],
+  },
+]
+
+
+
+
+//ashok family
+export const tree2 = [
+  {
+    id: 56,
+    person: {
+      id: 56,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Sheetal Sankonatti',
+      title: 'Electrical engineer',
+      totalReports: 0,
+      link: 'https://github.com/unicef/react-org-chart',
+    },
+    hasChild: true,
+    hasParent: true,
+    // children: [],
+  },
+  {
+    id: 57,
+    person: {
+      id: 57,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Shilpa sankonatti',
+      title: 'House wife',
+      totalReports: 0,
+      link: 'https://github.com/unicef/react-org-chart',
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 18,
+    person: {
+      id: 18,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'sammed.sankonatti',
+      title: 'Software developer',
+      totalReports: 0,
+      link: 'https://github.com/unicef/react-org-chart',
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  }
+]
+
+export const tree3 = [
+  {
+    id: 70,
+    person: {
+      id: 70,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Kenneth dom',
+      title: 'IT Officer',
+      totalReports: 0,
+    },
+    hasChild: false,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 45,
+    person: {
+      id: 45,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Kin baker',
+      title: 'IT Officer',
+      totalReports: 0,
+    },
+    hasChild: false,
+    hasParent: true,
+    children: [],
+  },
+]
+
+
+//ningappa family members
+export const tree4 = [
+  {
+    id: 102,
+    person: {
+      id: 102,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Ashwini Sankonatti',
+      title: 'House wife',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+ {
+    id: 103,
+    person: {
+      id: 103,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Archana Sankonatti',
+      title: 'House wife',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 104,
+    person: {
+      id: 104,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'kanchana Sankonatti',
+      title: '2nd PUC',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 105,
+    person: {
+      id: 105,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Savita Sankonatti',
+      title: 'House wife',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 106,
+    person: {
+      id: 106,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Pallavi Sankonatti',
+      title: '2nd PUC',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 107,
+    person: {
+      id: 107,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Sanjana Sankonatti',
+      title: '10th Std',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+]
+
+
+//suresh family members
+export const tree5 = [
+  {
+    id: 108,
+    person: {
+      id: 108,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Supriya Sankonatti',
+      title: 'House wife',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 109,
+    person: {
+      id: 109,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Sachin Sankonatti',
+      title: 'Software engineer',
+      totalReports: 0,
+    },
+    hasChild: false,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 110,
+    person: {
+      id: 110,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Sammed Sankonatti',
+      title: 'self employed',
+      totalReports: 0,
+    },
+    hasChild: false,
+    hasParent: true,
+    children: [],
+  },
+
+  {
+    id: 111,
+    person: {
+      id: 111,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Pavan Sankonatti',
+      title: '12th std',
+      totalReports: 0,
+    },
+    hasChild: false,
+    hasParent: true,
+    children: [],
+  },
+]
+
+//shreekant family members
+export const tree6 = [
+  {
+    id: 112,
+    person: {
+      id: 102,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Sushmita Sankonatti',
+      title: '2nd PUC done',
+      totalReports: 0,
+    },
+    hasChild: true,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 113,
+    person: {
+      id: 113,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Sapna Sankonatti',
+      title: '2nd PUC done',
+      totalReports: 0,
+    },
+    hasChild: false,
+    hasParent: true,
+    children: [],
+  },
+  {
+    id: 114,
+    person: {
+      id: 114,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Shreedhar Sankonatti',
+      title: '2nd PUC',
+      totalReports: 0,
+    },
+    hasChild: false,
+    hasParent: true,
+    children: [],
+  },
+
+  {
+    id: 115,
+    person: {
+      id: 115,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Shashidhar Sankonatti',
+      title: '10th std',
+      totalReports: 0,
+    },
+    hasChild: false,
+    hasParent: true,
+    children: [],
+  },
+]
+
+
+//nishkant family members
+export const tree7 = [
+  {
+    id: 116,
+    person: {
+      id: 116,
+      avatar: avatarPersonnel,
+      department: '',
+      name: 'Pradeep Sankonatti',
+      title: 'Engineering student',
+      totalReports: 0,
+    },
+    hasChild: false,
+    hasParent: true,
+    children: [],
+  }
+]
